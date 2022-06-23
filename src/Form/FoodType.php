68204a861b62cef39f3d3e5be35068ca2b95b3ae
lecaohao211 <102611398+lecaohao211@users.noreply.github.com>
2022-06-23 15:44:51 +0700

    create form

---
+++ src/Form/FoodType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Food;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FoodType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Name')
+            ->add('Price')
+            ->add('ChefID')
+            ->add('CategoryID')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Food::class,
+        ]);
+    }
+}
