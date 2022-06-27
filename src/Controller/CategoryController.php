a5d41941bb55f122656f084d6a2a418fd92215c8
lecaohao211 <102611398+lecaohao211@users.noreply.github.com>
2022-06-27 16:09:17 +0700

    indexfood

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -1,116 +1,116 @@
 <?php
-
-namespace App\Controller;
-
-use App\Entity\Category;
-use App\Form\CategoryType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-
-class CategoryController extends AbstractController
-{
-
-
-    /**
-     * @Route("/category", name="app_category")
-     */
-    public function index()
-    {
-        $categories = $this->getDoctrine()
-            ->getRepository(Category::class)
-            ->findAll();
-        return $this->render('category/index.html.twig', [
-            'categories' => $categories,
-        ]);
-    }
-    /**
-     * @Route("/CategoryController/details/{id}", name="category_details")
-     */
-    public
-    function detailsAction($id)
-    {
-        $categories = $this->getDoctrine()
-            ->getRepository(Category::class)
-            ->find($id);
-
-        return $this->render('category/details.html.twig', [
-            'categories' => $categories,
-        ]);
-    }
-    /**
-     * @Route("/category/create", name="category_create", methods={"GET","POST"})
-     */
-    public function createAction(Request $request)
-    {
-        $category = new Category();
-        $form = $this->createForm(CategoryType::class, $category);
-
-        if ($this->saveChanges($form, $request, $category)) {
-            $this->addFlash(
-                'notice',
-                'Category Added'
-            );
-
-            return $this->redirectToRoute('app_category');
-        }
-
-        return $this->render('category/create.html.twig', [
-            'form' => $form->createView()
-        ]);
-    }
-
-    public function saveChanges($form, $request, $category)
-    {
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $category->setName($request->request->get('category')['name']);
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($category);
-            $em->flush();
-
-            return true;
-        }
-        return false;
-    }
-    /**
-     * @Route("/category/edit/{id}", name="category_edit")
-     */
-    public function editAction($id, Request $request)
-    {
-        $em = $this->getDoctrine()->getManager();
-        $category = $em->getRepository(Category::class)->find($id);
-
-        $form = $this->createForm(CategoryType::class, $category);
-
-        if ($this->saveChanges($form, $request, $category)) {
-            $this->addFlash(
-                'notice',
-                'Category Edited'
-            );
-            return $this->redirectToRoute('app_category');
-        }
-        return $this->render('category/edit.html.twig', [
-            'form' => $form->createView()
-        ]);
-    }
-
-    /**
-     * @Route("/category/delete/{id}", name="category_delete")
-     */
-    public function deleteAction($id)
-    {
-        $em = $this->getDoctrine()->getManager();
-        $category = $em->getRepository(Category::class)->find($id);
-        $em->remove($category);
-        $em->flush();
-
-        $this->addFlash(
-            'error',
-            'Category deleted'
-        );
-        return $this->redirectToRoute('app_category');
-    }
-}
+//
+//namespace App\Controller;
+//
+//use App\Entity\Category;
+//use App\Form\CategoryType;
+//use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+//use Symfony\Component\HttpFoundation\Request;
+//use Symfony\Component\HttpFoundation\Response;
+//use Symfony\Component\Routing\Annotation\Route;
+//
+//class CategoryController extends AbstractController
+//{
+//
+//
+//    /**
+//     * @Route("/category", name="app_category")
+//     */
+//    public function index()
+//    {
+//        $categories = $this->getDoctrine()
+//            ->getRepository(Category::class)
+//            ->findAll();
+//        return $this->render('category/index.html.twig', [
+//            'categories' => $categories,
+//        ]);
+//    }
+//    /**
+//     * @Route("/CategoryController/details/{id}", name="category_details")
+//     */
+//    public
+//    function detailsAction($id)
+//    {
+//        $categories = $this->getDoctrine()
+//            ->getRepository(Category::class)
+//            ->find($id);
+//
+//        return $this->render('category/details.html.twig', [
+//            'categories' => $categories,
+//        ]);
+//    }
+//    /**
+//     * @Route("/category/create", name="category_create", methods={"GET","POST"})
+//     */
+//    public function createAction(Request $request)
+//    {
+//        $category = new Category();
+//        $form = $this->createForm(CategoryType::class, $category);
+//
+//        if ($this->saveChanges($form, $request, $category)) {
+//            $this->addFlash(
+//                'notice',
+//                'Category Added'
+//            );
+//
+//            return $this->redirectToRoute('app_category');
+//        }
+//
+//        return $this->render('category/create.html.twig', [
+//            'form' => $form->createView()
+//        ]);
+//    }
+//
+//    public function saveChanges($form, $request, $category)
+//    {
+//        $form->handleRequest($request);
+//
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $category->setName($request->request->get('category')['name']);
+//            $em = $this->getDoctrine()->getManager();
+//            $em->persist($category);
+//            $em->flush();
+//
+//            return true;
+//        }
+//        return false;
+//    }
+//    /**
+//     * @Route("/category/edit/{id}", name="category_edit")
+//     */
+//    public function editAction($id, Request $request)
+//    {
+//        $em = $this->getDoctrine()->getManager();
+//        $category = $em->getRepository(Category::class)->find($id);
+//
+//        $form = $this->createForm(CategoryType::class, $category);
+//
+//        if ($this->saveChanges($form, $request, $category)) {
+//            $this->addFlash(
+//                'notice',
+//                'Category Edited'
+//            );
+//            return $this->redirectToRoute('app_category');
+//        }
+//        return $this->render('category/edit.html.twig', [
+//            'form' => $form->createView()
+//        ]);
+//    }
+//
+//    /**
+//     * @Route("/category/delete/{id}", name="category_delete")
+//     */
+//    public function deleteAction($id)
+//    {
+//        $em = $this->getDoctrine()->getManager();
+//        $category = $em->getRepository(Category::class)->find($id);
+//        $em->remove($category);
+//        $em->flush();
+//
+//        $this->addFlash(
+//            'error',
+//            'Category deleted'
+//        );
+//        return $this->redirectToRoute('app_category');
+//    }
+//}
--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -1,36 +1,36 @@
 <?php
-
-namespace App\Form;
-
-use App\Entity\Category;
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-
-class CategoryType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('name', ChoiceType::class, [
-                    'choices' => [
-                        'Appetisers' => "Appetisers",
-                        'Mains' => "Mains",
-                        'Desserts' => "Desserts",
-                        'Drinks'=>"Drinks",
-                    ]
-                ]
-            );
-    }
-
-
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefaults([
-            'data_class' => Category::class,
-        ]);
-    }
-
-}
+//
+//namespace App\Form;
+//
+//use App\Entity\Category;
+//use Symfony\Component\Form\AbstractType;
+//use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+//use Symfony\Component\Form\Extension\Core\Type\TextType;
+//use Symfony\Component\Form\FormBuilderInterface;
+//use Symfony\Component\OptionsResolver\OptionsResolver;
+//
+//class CategoryType extends AbstractType
+//{
+//    public function buildForm(FormBuilderInterface $builder, array $options)
+//    {
+//        $builder
+//            ->add('name', ChoiceType::class, [
+//                    'choices' => [
+//                        'Appetisers' => "Appetisers",
+//                        'Mains' => "Mains",
+//                        'Desserts' => "Desserts",
+//                        'Drinks'=>"Drinks",
+//                    ]
+//                ]
+//            );
+//    }
+//
+//
+//    public function configureOptions(OptionsResolver $resolver): void
+//    {
+//        $resolver->setDefaults([
+//            'data_class' => Category::class,
+//        ]);
+//    }
+//
+//}
