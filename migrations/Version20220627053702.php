352c9572dc37ac2a8d2c7e9b88ea9292b38dc913
Your Name <you@example.com>
2022-06-28 17:48:08 +0700

    Modify the style form of view table

---
+++ migrations/Version20220627053702.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220627053702 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE chef (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, gender LONGTEXT NOT NULL, salary DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE food (id INT AUTO_INCREMENT NOT NULL, chef_id_id INT NOT NULL, category_id_id INT NOT NULL, name VARCHAR(255) NOT NULL, price DOUBLE PRECISION NOT NULL, INDEX IDX_D43829F7A32F0D1 (chef_id_id), INDEX IDX_D43829F79777D11E (category_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE food ADD CONSTRAINT FK_D43829F7A32F0D1 FOREIGN KEY (chef_id_id) REFERENCES chef (id)');
+        $this->addSql('ALTER TABLE food ADD CONSTRAINT FK_D43829F79777D11E FOREIGN KEY (category_id_id) REFERENCES category (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE food DROP FOREIGN KEY FK_D43829F79777D11E');
+        $this->addSql('ALTER TABLE food DROP FOREIGN KEY FK_D43829F7A32F0D1');
+        $this->addSql('DROP TABLE category');
+        $this->addSql('DROP TABLE chef');
+        $this->addSql('DROP TABLE food');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
